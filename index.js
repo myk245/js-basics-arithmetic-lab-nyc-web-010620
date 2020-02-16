7baf15bc5919f2094dada628ac1dff2d2e762ece
myk245 <myk245@nyu.edu>
2020-02-16 00:36:34 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here
+const var1 = 2; 
+const var2 = 31; 
+const multiply = var1 * var2;
+
+const random = Math.random();
+
+const mod = 34 % 5; 
+
+const max = Math.max(1, 20, 5, 8)
